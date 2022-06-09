5ec581beffc8c48a8ab5544485e4b9ec25d5f308
Erik Koopmans <erik.koopmans@d2l.com>
2022-06-09 13:48:50 -0400

    Begin adding guidepup into npm test

--- src/test/demo.test.js
+++ src/test/demo.test.js
@@ -1,5 +1,8 @@
 import '../demo.js';
 import { expect, fixture, html } from '@open-wc/testing';
+// NOTE: This import breaks in web-test-runner:
+//	SyntaxError: The requested module './../../node_modules/@guidepup/guidepup/lib/index.js' does not provide an export named 'MacOSApplications'
+// import { run } from './guidepup-demo.js';
 
 describe('wc-demo', () => {
 
@@ -7,5 +10,11 @@ describe('wc-demo', () => {
 		const elem = await fixture(html`<wc-demo>Demo</wc-demo>`);
 		expect(elem.textContent).to.equal('Hello World');
 	});
+
+	// it('does voiceover', () => {
+	// 	const { text, spoken } = run();
+	// 	console.log(text);
+	// 	console.log(spoken);
+	// });
 
 });
---
+++ src/test/guidepup-demo.js
@@ -0,0 +1,61 @@
+import {
+  macOSActivate,
+  MacOSApplications,
+  macOSQuit,
+  voiceOver,
+} from '@guidepup/guidepup';
+
+const delay = async (ms) =>
+  await new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Opens Safari and navigates to the guidepup GitHub repo.
+ */
+export async function run() {
+  const output = {};
+
+  try {
+    // Start the VoiceOver screenreader
+    await voiceOver.start();
+
+    // Open Safari and wait for it to be ready.
+    //
+    // Not best practice, but expectation is that consumer will perform proper
+    // checks to ensure that Safari is ready, most likely using something like
+    // playwright to launch and control the browser.
+    await macOSActivate(MacOSApplications.Safari);
+    await delay(4000);
+
+    // Interact with the toolbar.
+    await voiceOver.interact();
+
+    // Navigate across to the address input.
+    await voiceOver.next();
+    await voiceOver.next();
+    await voiceOver.next();
+    await voiceOver.next();
+    await voiceOver.next();
+
+    // Navigate to guidepup repo.
+    await voiceOver.type("https://github.com/guidepup/guidepup");
+    await voiceOver.act();
+
+    console.log("Item Text Log:");
+    console.log(await voiceOver.itemTextLog());
+    console.log("Spoken Phrase Log:");
+    console.log(await voiceOver.spokenPhraseLog());
+
+    output.text = voiceOver.itemTextLog();
+    output.spoken = voiceOver.spokenPhraseLog();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // Ensure we stop VoiceOver.
+    await voiceOver.stop();
+
+    // Ensure we quit Safari.
+    await macOSQuit(MacOSApplications.Safari);
+  }
+
+  return output;
+}
